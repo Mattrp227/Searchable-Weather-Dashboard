ef4c1f9cd5773bf28d75a18ae58b8ae50ed151d2
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-08-24 17:57:05 -1000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,39 @@
 //Get weather api
 var WeatherAPI = "18c3ec4275b5f7e164ac10b88d590ea6";
 var APIKey = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={minutely,hourly,alerts}&appid=18c3ec4275b5f7e164ac10b88d590ea6;
+
+//Create variables from html for search button
+var searchButton = $("searchbutton");
+
+//Current Weather Conditions of a city
+function currentWeather (
+    //call api
+
+    //search for city
+
+)
+
+var uvindex = ;
+//uv index color change
+
+if(uvindex < 4) {
+ //color yellow
+}
+
+else if (4 < uvindex < 7) {
+    //color orange
+}
+else if (uvindex > 7) {
+    //color red
+
+}
+
+//five day forecast
+function futureWeather (
+    //call api
+)
+
+//create local storage for history
+function getHistory (
+    
+)
