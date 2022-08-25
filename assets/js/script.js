7f10bc6c22bd0fa997391293bac9404f4b3ddc5d
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-08-24 17:32:08 -1000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,3 @@
-
+//Get weather api
+var WeatherAPI = "18c3ec4275b5f7e164ac10b88d590ea6";
+var APIKey = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={minutely,hourly,alerts}&appid=18c3ec4275b5f7e164ac10b88d590ea6;
